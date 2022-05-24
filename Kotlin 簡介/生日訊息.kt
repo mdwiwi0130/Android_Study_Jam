7775252b79413468de69a8e0b2c48fc993d562c6
naiye130 <65766065+mdwiwi0130@users.noreply.github.com>
2022-05-24 20:43:31 +0800

    Create 生日訊息.kt

---
+++ Kotlin 簡介/生日訊息.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val
+    println("Happy Birthday, Rover!")
+    
+    // This is a commenat line
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    println("")
+    
+    // This is another comment
+    print("You are already 5!\n")
+    print("5 is the very best age to celebrate!\n")
+}
